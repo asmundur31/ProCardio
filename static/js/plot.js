42d0a6d116e966741187519c90a3649dd2edf93b
Ásmundur Óskar Ásmundsson <asmundur31@gmail.com>
2022-12-06 12:19:17 +0100

    Added heart beat data

--- static/js/plot.js
+++ static/js/plot.js
@@ -8,15 +8,26 @@ var data = jsonO.recording;
 
 
 let speedArr = [];
- data.treadmill.measurements.FTMS.forEach(item => {
-   speedArr.push(item.speed)
- });
+data.treadmill.measurements?.FTMS?.forEach(item => {
+  speedArr.push(item.speed)
+});
 
 let distanceArr = [];
- data.treadmill.measurements.FTMS.forEach(item => {
-    distanceArr.push(item.distance)
- });
+data.treadmill.measurements?.FTMS?.forEach(item => {
+  distanceArr.push(item.distance)
+});
 
+let hrArr = [];
+let hrTimeArr = [];
+data.hr.measurements?.HR?.forEach(item => {
+  hrArr.push(item.heartRate);
+  hrTimeArr.push(item.time);
+});
+
+var newHRTime = [];
+for(var i = 0; i < hrTimeArr.length; i++) {
+  newHRTime.push((hrTimeArr[i] - hrTimeArr[0])/1000);
+}
 
 var newSpeed = JSON.parse("[" + speedArr.join() + "]");
 //Remove the quotation mark from orignal speed data
@@ -43,23 +54,22 @@ console.log(newSpeed);
 
 ///////////////////////////////////////////////////////////////
 
-  const Speed = {
+const Speed = {
+  labels: timestamp,
+  datasets: [{
+    label: 'Speed',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointRadius: 0,
+    data: newSpeed,
+  }]
+};
 
-    labels: timestamp,
-    datasets: [{
-      label: 'Speed',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      pointRadius: 0,
-      data: newSpeed,
-    }]
-  };
-
-   const config = {
-    type: 'line',
-    data: Speed,
-    options: {
-      scales: {
+const config = {
+  type: 'line',
+  data: Speed,
+  options: {
+    scales: {
       x: {
         title: {
           display: true,
@@ -72,46 +82,77 @@ console.log(newSpeed);
           text: 'Speed (m/s)'
         }
       }
-    }     },
-  };
+    }
+  },
+};
 
+const Distance = {
+  labels: timestamp,
+  datasets: [{
+    label: 'Distance',
+    backgroundColor: 'rgb(50, 205, 50)',
+    borderColor: 'rgb(50, 205, 50)',
+    pointRadius: 0,
+    data: newDistance,
+  }]
+};
 
-  const Distance = {
-
-    labels: timestamp,
-    datasets: [{
-      label: 'Distance',
-      backgroundColor: 'rgb(50, 205, 50)',
-      borderColor: 'rgb(50, 205, 50)',
-      pointRadius: 0,
-      data: newDistance,
-    }]
-  };
-
-   const config1 = {
-    type: 'line',
-    data: Distance,
-    options: {
-      scales: {
-        x: {
-          title: {
-            display: true,
-            text: 'Time (s)'
-          }
-        },
-        y: {
-          title: {
-            display: true,
-            text: 'Distance (m)'
-          }
+const config1 = {
+  type: 'line',
+  data: Distance,
+  options: {
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Time (s)'
         }
-      }     
-    },
-  };
-  
-  
-   const SpeedChart = new Chart(document.getElementById('SpeedChart'),config );
-   const DistanceChart = new Chart(document.getElementById('DistanceChart'),config1 );
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Distance (m)'
+        }
+      }
+    }     
+  },
+};
+
+const HR = {
+  labels: newHRTime,
+  datasets: [{
+    label: 'Heart Rate',
+    backgroundColor: 'rgb(50, 50, 205)',
+    borderColor: 'rgb(50, 50, 205)',
+    pointRadius: 0,
+    data: hrArr,
+  }]
+};
+
+const configHR = {
+  type: 'line',
+  data: HR,
+  options: {
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Time (s)'
+        }
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Heart Rate (bpm)'
+        }
+      }
+    }     
+  },
+}
+
+const SpeedChart = new Chart(document.getElementById('SpeedChart'), config);
+const DistanceChart = new Chart(document.getElementById('DistanceChart'), config1);
+const HeartRateChart = new Chart(document.getElementById('HeartRateChart'), configHR);
 
 
   
